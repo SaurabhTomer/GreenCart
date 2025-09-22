9eb7730bb179492a9969882f3cc51d5a4d57a147
Saurabh Tomer <tomersaurabh91@gmail.com>
2025-09-22 10:50:39 +0530

    Fix input handling for email and password fields in SellerLogin component

--- src/components/seller/SellerLogin.jsx
+++ src/components/seller/SellerLogin.jsx
@@ -34,6 +34,8 @@ const SellerLogin = () => {
           <div className="w-full">
             <p>Email</p>
             <input
+              onChange={(e) => setEmail(e.target.value)}
+              value={email}
               type="email"
               placeholder="Enter your email"
               className="border border-gray-200 rounded w-full p-2 mt-1 outline-primary"
@@ -41,6 +43,8 @@ const SellerLogin = () => {
             />
             <p>Password</p>
             <input
+              onChange={(e) => setPassword(e.target.value)}
+              value={password}
               type="password"
               placeholder="Enter your password"
               className="border border-gray-200 rounded w-full p-2 mt-1 outline-primary"
